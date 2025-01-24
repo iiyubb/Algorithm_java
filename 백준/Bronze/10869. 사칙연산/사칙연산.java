58b186ce958c08b340ce87f0e283b344c74592d6
leeyub <80386881+iiyubb@users.noreply.github.com>
2025-01-24 13:42:47 +0900

    [Bronze V] Title: 사칙연산, Time: 168 ms, Memory: 17748 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int input1 = input.nextInt();
+		int input2 = input.nextInt();
+		
+		System.out.println(input1 + input2);
+		System.out.println(input1 - input2);
+		System.out.println(input1 * input2);
+		System.out.println(input1 / input2);
+		System.out.println(input1 % input2);
+
+	}
+
+}
