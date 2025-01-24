fc628af3006e716242093976c744d381677bb672
leeyub <80386881+iiyubb@users.noreply.github.com>
2025-01-24 13:43:19 +0900

    [Bronze V] Title: ??!, Time: 172 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		String inputS = input.nextLine();
+		System.out.println(inputS + "??!");
+
+	}
+
+}
