649138e3d4158927334c48f61579774f86bf4c89
leeyub <80386881+iiyubb@users.noreply.github.com>
2025-01-24 13:43:50 +0900

    [Bronze V] Title: A+B - 4, Time: 120 ms, Memory: 15880 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		String input;
+		
+		while ((input = br.readLine()) != null) {
+			int a = Integer.parseInt(input.split(" ")[0]);
+			int b = Integer.parseInt(input.split(" ")[1]);
+			
+			bw.write(a + b + "\n");
+		}
+		
+		bw.flush();
+		bw.close();
+			
+	}
+
+}
