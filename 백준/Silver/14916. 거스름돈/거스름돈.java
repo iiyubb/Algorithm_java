dfc6fa31596521bb819672f30cc874d0f16fc687
leeyub <80386881+iiyubb@users.noreply.github.com>
2025-02-18 10:42:41 +0900

    [Silver V] Title: 거스름돈, Time: 108 ms, Memory: 14364 KB -BaekjoonHub

---
+++ 백준/Silver/14916. 거스름돈/거스름돈.java
@@ -0,0 +1,27 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        if (n < 5 && n % 2 != 0) {
+            bw.write(-1+"");
+        } else {
+            int k = n / 5;
+            int m = n % 5;
+
+            if (m % 2 == 0) {
+                bw.write((m/2 + k) + "");
+            } else {
+                bw.write(((k-1) + (m+5)/2) + "");
+            }
+        }
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
