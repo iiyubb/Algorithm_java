245460f8cc5a8deacadd1a12e76d15ce46e0145d
leeyub <80386881+iiyubb@users.noreply.github.com>
2025-09-16 01:33:17 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 104 ms, Memory: 14176 KB -BaekjoonHub

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		LinkedList<Integer> queue = new LinkedList<>();
+		StringBuilder sb = new StringBuilder();
+		sb.append("<");
+		
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+		
+		for (int i=1; i<=n; i++) {
+		    queue.add(i);
+		}
+		
+		int i = 0;
+		while(!queue.isEmpty()) {
+		    i = (i + k - 1) % queue.size();
+		    sb.append(queue.get(i));
+		    queue.remove(i);
+		    
+		    if (!queue.isEmpty()) {
+		        sb.append(", ");
+		    }
+		}
+        sb.append(">");
+        System.out.print(sb.toString());
+	} 
+}
